33a6b294a17fd7e9a71ab4a37b436368091ad2c0
joshua-mcfield99 <72096757+joshua-mcfield99@users.noreply.github.com>
2023-05-08 21:58:11 +0100

    Revert "Change image through cloudinary"
    This reverts commit bfe2ef4556247377c71d1a20e2ad9ab6974d807f.

--- pages/Drinks.js
+++ pages/Drinks.js
@@ -1,44 +1,50 @@
-import Head from 'next/head';
-import React from 'react';
-import styles from '../styles/Drinks.module.css';
-import { Cormorant_Unicase } from 'next/font/google';
-import { Image } from 'cloudinary-react';
-import altText from '../data/liveBarAlts.json';
+import Head from 'next/head'
+import React from 'react'
+import styles from '../styles/Drinks.module.css'
+import { Cormorant_Unicase } from 'next/font/google'
+import Image from 'next/image'
+import altText from '../data/liveBarAlts.json'
 
 const cormorantUnicase = Cormorant_Unicase({
     subsets: ['latin'],
     weight: '700'
-});
+})
 
 const Drinks = () => {
 
     let alt = JSON.stringify(altText[0]);
     console.log(alt);
 
-    return (
-        <>
-            <Head>
-                <title>The Star | Drinks</title>
-            </Head>
-            <main>
-                <section>
-                    <h1 className={`${'heading'} ${cormorantUnicase.className}`} >Drinks</h1>
-                    <p>
-                        Looking for the perfect pint? Look no further than The Star! With our passion for beer and cider, we offer an ever-changing selection of up to 15 real ales and over 35 ciders. You&#39;ll be spoilt for choice, and your palate will be satisfied. Come join us and discover your new favourite drink!
-                    </p>
-                </section>
-                <section>
-                    <h2 className={`${'heading'} ${cormorantUnicase.className}`} >Live Bar</h2>
-                    <p>
-                        While we endeavor to maintain an up-to-date bar image, during peak hours, we may not always be able to do so.
-                    </p>
-                    <div className={`${styles.live_img}`}>
-                        <Image cloudName="drkqjlsvr" publicId="/livebarimage/LiveBarImage" alt='This is the live bar image it displays our current ales which is updated daily.' style={{width: '100%'}}/>
-                    </div>
-                </section>
-            </main>
-        </>
-    );
-};
+  return (
+    <>
+        <Head>
 
-export default Drinks;
+            <title>The Star | Drinks</title>
+        </Head>
+        <main>
+            <section>
+                <h1 className={`${'heading'} ${cormorantUnicase.className}`} >Drinks</h1>
+                <p>
+                    Looking for the perfect pint? Look no further than The Star! With our passion for beer and cider, we offer an ever-changing selection of up to 15 real ales and over 35 ciders. You&#39;ll be spoilt for choice, and your palate will be satisfied. Come join us and discover your new favourite drink!
+                </p>
+            </section>
+            <section>
+                <h2 className={`${'heading'} ${cormorantUnicase.className}`} >Live Bar</h2>
+                <p>
+                    While we endeavor to maintain an up-to-date bar image, during peak hours, we may not always be able to do so.
+                </p>
+                <div className={`${styles.live_img}`}>
+                    <Image
+                        src='/uploads/Live_Bar/LiveBar.png'
+                        alt={alt}
+                        fill
+                        className={`${styles.img_fill}`}
+                    />
+                </div>
+            </section>
+        </main>
+    </>
+  )
+}
+
+export default Drinks
