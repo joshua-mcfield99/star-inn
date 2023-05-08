bfe2ef4556247377c71d1a20e2ad9ab6974d807f
joshua-mcfield99 <72096757+joshua-mcfield99@users.noreply.github.com>
2023-05-08 21:54:20 +0100

    Change image through cloudinary

--- components/editors/LiveBarEditor.js
+++ components/editors/LiveBarEditor.js
@@ -1,67 +1,51 @@
 import React, { useState } from 'react'
 import styles from '/styles/Editors.module.css'
 import axios from 'axios';
+import cloudinary from 'cloudinary-core';
+
+
+
 
 
 const LiveBarEditor = () => {
+
+    const handleFileUpload = async (event) => {
+        const file = event.target.files[0];
+        const formData = new FormData();
+        formData.append('file', file);
+        formData.append('upload_preset', 'ez2vk527');
+        formData.append('public_id', 'LiveBarImage');
+        const response = await fetch(`https://api.cloudinary.com/v1_1/drkqjlsvr/upload`, {
+          method: 'POST',
+          body: formData
+        });
+        const data = await response.json();
+        console.log(data);
+        console.log(data.secure_url);
+    }
+
     const [newImage, setNewImage] = useState(null);
     const [altText, setAltText] = useState('');
     const [message, setMessage] = useState('');
-  
-    const handleSubmit = async (e) => {
-      e.preventDefault();
     
-      const formData = new FormData();
-      formData.append('newImage', newImage);
-      formData.append('altText', altText);
-  
-      try {
-        const response = await axios.post('/api/change-live-bar-image', formData);
-        setMessage(response.data.message);
-      } catch (error) {
-        console.error(error);
-        setMessage('Error uploading file');
-      }
-    };
-  
-    const handleFileChange = (e) => {
-      setNewImage(e.target.files[0]);
-      
-    };
-  
-    const handleAltTextChange = (e) => {
-      setAltText(e.target.value);
-    };
-
+   
+    
     return (
         <section className={`${styles.editor_section}`}>
-            <form className={`${styles.editor_form}`} onSubmit={handleSubmit} method="post" action="/api/upload" encType="multipart/form-data">
-                <div className={`${styles.form_control}`}>
-                    <label htmlFor='newImage'>
-                        Choose a live bar image.
-                    </label>
-                    <input 
-                        name='newImage'
-                        type='file'
-                        onChange={handleFileChange}
-                    />
-                </div>
-                <div className={`${styles.form_control}`}>
-                    <label htmlFor='altText'>
-                        Write a short sentence introducing the ales in the photo.
-                    </label>
-                    <textarea
-                        name='altText'
-                        type='text'
-                        rows={4}
-                        onChange={handleAltTextChange}
-                    />
-                </div>
-                <button>Submit</button>
-                {message && <p>{message}</p>}
-            </form>
+        <form className={`${styles.editor_form}`} method="post" action="/api/upload" encType="multipart/form-data">
+        <div className={`${styles.form_control}`}>
+        <label htmlFor='newImage'>
+        Choose a live bar image.
+        </label>
+        <input 
+        name='newImage'
+        type='file'
+        onChange={handleFileUpload}
+        />
+        </div>
+        </form>
         </section>
-    )
-}
+        )
+    }
     
-export default LiveBarEditor
+    export default LiveBarEditor
