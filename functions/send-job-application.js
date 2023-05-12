0acb17b52f67d93f8470e6a29ad0cc19a827019f
joshua-mcfield99 <72096757+joshua-mcfield99@users.noreply.github.com>
2023-05-12 15:34:31 +0100

    Change nodemailer.js imports

--- functions/send-job-application.js
+++ functions/send-job-application.js
@@ -1,4 +1,4 @@
-import { mailOptions, transporter } from "@/config/nodemailer";
+import { mailOptions, transporter } from "../config/nodemailer";
 
 const CONTACT_MESSAGE_FIELDS = {
     name: "Name",
