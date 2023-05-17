e18c067706b9fb23ae204f36119951f05ef08308
joshua-mcfield99 <72096757+joshua-mcfield99@users.noreply.github.com>
2023-05-17 11:43:38 +0100

    Improve error handling for tomorrow

--- functions/api.js
+++ functions/api.js
@@ -1,11 +1,13 @@
-exports.sendJobApplication = async (data) => fetch('https://www.starinngodalming.co.uk/.netlify/functions/send-job-application', {
-    method: "POST",
-    body: JSON.stringify(data),
-    headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json"
-    }
-}).then((res) => {
-    if(!res.ok) throw new Error("Failed to send message");
-    return res.json();
-});
+export const sendJobApplication = async (data) => {
+    return fetch("https://www.starinngodalming.co.uk/.netlify/functions/send-job-application", {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+        },
+    }).then((res) => {
+        if (!res.ok) throw new Error("Failed to send message");
+        return res.json();
+    });
+};
