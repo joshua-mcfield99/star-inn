ebe79f70dd50cd6ba36e51f6b9dc8b680503bbb2
joshua-mcfield99 <72096757+joshua-mcfield99@users.noreply.github.com>
2023-06-21 10:58:09 +0100

    Add GBW contact and info

--- pages/Drinks.js
+++ pages/Drinks.js
@@ -15,7 +15,6 @@ const Drinks = () => {
   return (
     <>
         <Head>
-
             <title>The Star | Drinks</title>
         </Head>
         <main>
@@ -24,6 +23,20 @@ const Drinks = () => {
                 <p>
                     Looking for the perfect pint? Look no further than The Star! With our passion for beer and cider, we offer an ever-changing selection of up to 15 real ales and over 35 ciders. You&#39;ll be spoilt for choice, and your palate will be satisfied. Come join us and discover your new favourite drink!
                 </p>
+            </section>
+            <section>
+                <h2 className={`${'heading'} ${cormorantUnicase.className}`} >Godalming Beer Works</h2>
+                <p>Welcome to our microbrewery, where we craft exceptional ales to satisfy every beer enthusiast. Our lineup includes:</p>
+                <ul className={`${styles.gbw_drinks}`}>
+                    <li><strong>First Light:</strong> An American IPA with vibrant citrus and pine flavors.</li>
+                    <li><strong>Ravenscroft:</strong> A modern twist on the best bitter, combining caramel sweetness and earthy hops.</li>
+                    <li><strong>Pepper Pot Porter:</strong> A velvety brew with hints of coconut and vanilla, offering a touch of exotic allure.</li>
+                    <li><strong>Wagon and Horses:</strong> An Amber IPA blending toasty malt sweetness with tropical fruit flavors.</li>
+                </ul>
+                <p>
+                    Experience the artistry behind each pint we pour as we push the boundaries of beer craftsmanship. From hoppy IPAs to rich porters, we offer a range of flavors to elevate your beer-drinking experience. Join us in celebrating the perfect blend of tradition and innovation. Cheers to great beer!
+                </p>
+                <p>Want to find out more? Check out the contact page about how to get intouch with GBW (Godalming Beer Works).</p>
             </section>
             <section>
                 <h2 className={`${'heading'} ${cormorantUnicase.className}`} >Live Bar</h2>
